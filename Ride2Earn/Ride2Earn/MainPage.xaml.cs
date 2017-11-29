7706f530fc6890015db6abd938c9c76d1253326a
LanternMyWilly <34011253+LanternMyWilly@users.noreply.github.com>
2017-11-29 19:35:29 +0100

    Add project files.

---
+++ Ride2Earn/Ride2Earn/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ride2Earn
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
