10d2a9d2ed0dbf1b5516f37474691401aca29445
LanternMyWilly <34011253+LanternMyWilly@users.noreply.github.com>
2018-01-17 17:06:40 +0100

    Custom Renderers aangepast

---
+++ Ride2Earn/Ride2Earn.Android/CustomRenderer/CustomEntryRenderer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Ride2Earn;
+using Ride2Earn.Droid.CustomRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ICustomEntryRenderer), typeof(CustomEntryRenderer))]
+namespace Ride2Earn.Droid.CustomRenderer
+{
+    public class CustomEntryRenderer : Xamarin.Forms.Platform.Android.EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var view = (ICustomEntryRenderer)Element;
+
+                if (view.IsCurvedCornersEnabled)
+                {
+                    // creating gradient drawable for the curved background
+                    var _gradientBackground = new GradientDrawable();
+                    _gradientBackground.SetShape(ShapeType.Rectangle);
+                    _gradientBackground.SetColor(view.CurvedBackgroundColor.ToAndroid());
+
+                    // Thickness of the stroke line
+                    _gradientBackground.SetStroke(view.BorderWidth, view.BorderColor.ToAndroid());
+
+                    // Radius for the curves
+                    _gradientBackground.SetCornerRadius(
+                        DpToPixels(this.Context,
+                            Convert.ToSingle(view.CornerRadius)));
+
+                    // set the background of the label
+                    Control.SetBackground(_gradientBackground);
+                }
+
+                // Set padding for the internal text from border
+                Control.SetPadding(
+                    (int)DpToPixels(this.Context, Convert.ToSingle(12)),
+                    Control.PaddingTop,
+                    (int)DpToPixels(this.Context, Convert.ToSingle(12)),
+                    Control.PaddingBottom);
+            }
+        }
+        public static float DpToPixels(Context context, float valueInDp)
+        {
+            DisplayMetrics metrics = context.Resources.DisplayMetrics;
+            return TypedValue.ApplyDimension(ComplexUnitType.Dip, valueInDp, metrics);
+        }
+    }
+}
